ed9ed50b9c362f10c00d060ae5d3dfc1460de682
TatuJLund <tatulund@live.com>
2024-03-30 22:40:04 +0200

    Added headers

--- src/test/java/com/vaadin/testbench/uiunittest/views/GridTestView.java
+++ src/test/java/com/vaadin/testbench/uiunittest/views/GridTestView.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2000-2024 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 package com.vaadin.testbench.uiunittest.views;
 
 import java.util.List;
@@ -6,7 +14,6 @@ import java.util.stream.IntStream;
 
 import com.vaadin.testbench.uiunittest.TestView;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
